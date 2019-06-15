8e57bae9d4023771d9fa52b4e31388efe88bc7d1
celeron1ghz <celeron1ghz@gmail.com>
2019-06-15 15:46:46 +0900

    change cert path

--- server/dev/update_sslcert.sh
+++ server/dev/update_sslcert.sh
@@ -1,8 +1,12 @@
 DOMAIN=10.0.1.6
-echo "generate cert for $DOMAIN..."
 
-cd `dirname $0`
+cd `dirname $0`/..
+mkdir -p .cert
+cd .cert
+echo "cert_dir: $(pwd)"
 rm -f *.pem
+
+echo "generate cert for $DOMAIN..."
 openssl genrsa 2048 > key.pem
 openssl req -subj "/C=JP/ST=Tokyo/L=Chuo-ku/O=Example K.K./OU=Example Section/CN=$DOMAIN" -new -key key.pem > csr.pem
 openssl x509 -days 3650 -req -signkey key.pem < csr.pem > cert.pem
